85e40f576fed6d4452321aa2515121d248dcd30b
David Datjko <datjko@gmail.com>
2016-05-08 16:06:14 -0700

    replaced cv::SimpleBlobDetector with my siilar detector, added publishing detected position on image

--- test_simple_detector_node.cpp
+++ test_simple_detector_node.cpp
@@ -8,8 +8,10 @@
 
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
+#include <geometry_msgs/Point.h>
 #include <sensor_msgs/image_encodings.h>
 #include <ros/ros.h>
+
 
 
 struct ObjMaskMaker {
@@ -32,11 +34,70 @@ struct ObjMaskMaker {
         cv::cvtColor(hsv_image, hsv_image, cv::COLOR_BGR2HSV);
         std::vector<cv::Mat> hsv_channels;
         cv::split(hsv_image, hsv_channels);
+//        cv::imshow("saturation", hsv_channels[1]);
+//        cv::waitKey();
         return (hsv_channels[1] < saturation_threshold) & (hsv_channels[2] >= brightness_threshold);
     }
 };
 
-typedef cv::SimpleBlobDetector BlobDetector;
+class BlobDetector {
+public:
+    struct Params {
+        int minSize;
+        int maxSize;
+        int minArea;
+        int maxArea;
+        int border_margin;
+
+        Params() {
+            minSize = 30;
+            maxSize = 200;
+            minArea = 600;
+            maxArea = 40000;
+            border_margin = 3;
+        }
+    };
+
+    typedef std::vector<cv::Point> Contour;
+    typedef std::vector<Contour> Contours;
+
+    Params params_;
+
+    Contours findBlobContours(cv::Mat const& binary_image) const {
+        Contours contours;
+        cv::Mat const tmp_binary_image = binary_image.clone();
+        cv::findContours(tmp_binary_image, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+        Contours result;
+        for (size_t contourIdx = 0; contourIdx < contours.size(); contourIdx++) {
+            if(checkBbox(contours[contourIdx], binary_image.size()) && checkArea(contours[contourIdx]))
+                result.push_back(contours[contourIdx]);
+        }
+        return result;
+    }
+
+private:
+    bool checkBbox(Contour const& contour, cv::Size const& image_size) const {
+        cv::Rect bbox = cv::boundingRect(contour);
+        return (checkSize(bbox.size())
+                && params_.border_margin < bbox.tl().x
+                && params_.border_margin < bbox.tl().y
+                && image_size.width > params_.border_margin + bbox.br().x
+                && image_size.height > params_.border_margin + bbox.br().y
+               );
+    }
+    bool checkSize(cv::Size const& contour_size) const {
+        bool res = (params_.minSize <= std::min(contour_size.height, contour_size.width)
+                    && params_.maxSize > std::max(contour_size.height, contour_size.width));
+        //ROS_INFO("contour_size: %d, %d. res: %d", contour_size.height, contour_size.width, res ? 1 : 0);
+        return res;
+    }
+    bool checkArea(Contour const& contour) const {
+        double area = cv::contourArea(contour);
+        //ROS_INFO("contour_area: %f", area);
+        return (params_.minArea <= area && params_.maxArea > area);
+    }
+};
+
 
 struct ResultImageMaker {
     struct Params {
@@ -47,7 +108,7 @@ struct ResultImageMaker {
 
     Params params_;
 
-    cv::Mat makeStitchedResultImage(cv::Mat const& src_image, cv::Mat const& obj_mask, std::vector<cv::KeyPoint> const& keypoints) const {
+    cv::Mat makeStitchedResultImage(cv::Mat const& src_image, cv::Mat const& obj_mask, BlobDetector::Contours const& blob_contours) const {
         int rows = src_image.size().height;
         int cols = src_image.size().width;
         cv::Mat stitch = cv::Mat(rows, 2 * cols + 2, CV_8UC3, cv::Scalar(0));
@@ -56,12 +117,12 @@ struct ResultImageMaker {
         src_image.copyTo(stitch.colRange(0, cols).rowRange(0, rows));
 
         //: right: highlited object:
-        cv::Mat res_image = makeResultImage(src_image, obj_mask, keypoints);
+        cv::Mat res_image = makeResultImage(src_image, obj_mask, blob_contours);
         res_image.copyTo(stitch.colRange(cols + 1, 2 * cols + 1).rowRange(0, rows));
         return stitch;
     }
 
-    cv::Mat makeResultImage(cv::Mat const& src_image, cv::Mat const& obj_mask, std::vector<cv::KeyPoint> const& keypoints) const {
+    cv::Mat makeResultImage(cv::Mat const& src_image, cv::Mat const& obj_mask, BlobDetector::Contours const& blob_contours) const {
         cv::Mat res_image;
         std::vector<cv::Mat> bgr_channels;
         cv::split(src_image, bgr_channels);
@@ -84,7 +145,12 @@ struct ResultImageMaker {
             cv::merge(hsv_channels, hsv_image);
             cv::cvtColor(hsv_image, res_image, cv::COLOR_HSV2BGR);
         }
-        cv::drawKeypoints(res_image, keypoints, res_image, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+        for(size_t i = 0; i < blob_contours.size(); ++i) {
+            cv::Rect bbox = cv::boundingRect(blob_contours[i]);
+            cv::rectangle(res_image, bbox, cv::Scalar(0,0,255));
+            cv::circle(res_image, cv::Point((bbox.br().x + bbox.tl().x) / 2, (bbox.br().y + bbox.tl().y) / 2), 3, cv::Scalar(0,0,255));
+        }
         return res_image;
     }
 
@@ -192,6 +258,7 @@ private:
 
     image_transport::ImageTransport it_;
     image_transport::Subscriber mono_image_sub_;
+    ros::Publisher res_pub_;
 
     ObjMaskMaker mask_maker_;
     ResultImageMaker result_image_maker_;
@@ -211,32 +278,18 @@ public:
         updateParam("input_image_topic",           input_image_topic);
         mono_image_sub_ = it_.subscribe(input_image_topic, 1, &TestSimpleImageProcessor::monoImageCb, this, image_transport::TransportHints(input_image_transfer_hint));
 
+        res_pub_ = nh.nh_.advertise<geometry_msgs::Point>("detector_result_as_point", 1);
+
         //: object detection params:
         updateParam("saturation_threshold", mask_maker_.params_.saturation_threshold_);
         updateParam("brightness_threshold", mask_maker_.params_.brightness_threshold_);
 
         //: candidate filtering params:
-        BlobDetector::Params blob_detector_params;
-//        blob_detector_params.minThreshold = 1;
-//        blob_detector_params.maxThreshold = 2;
-        blob_detector_params.filterByColor = true;
-        blob_detector_params.blobColor = 255;
-        blob_detector_params.filterByArea = true;
-        blob_detector_params.filterByInertia = false;
-        blob_detector_params.filterByConvexity = false;
-        blob_detector_params.minDistBetweenBlobs = 40;
-        updateParam("blob_filter_min_dist_between_blobs", blob_detector_params.minDistBetweenBlobs);
-        blob_detector_params.minArea = 40;
-        updateParam("blob_filter_min_blob_area", blob_detector_params.minArea);
-        blob_detector_params.maxArea = 90000;
-        updateParam("blob_filter_max_blob_area", blob_detector_params.maxArea);
-//        blob_detector_params.filterByCircularity = false;
-//        updateParam("blob_filter_use_circularity", blob_detector_params.filterByCircularity);
-//        blob_detector_params.minCircularity = 0;
-//        updateParam("blob_filter_min_circularity", blob_detector_params.minCircularity);
-//        blob_detector_params.maxCircularity = 1;
-//        updateParam("blob_filter_max_circularity", blob_detector_params.maxCircularity);
-        blob_detector_ = BlobDetector(blob_detector_params);
+        updateParam("blob_filter_min_blob_size", blob_detector_.params_.minSize);
+        updateParam("blob_filter_max_blob_size", blob_detector_.params_.maxSize);
+        updateParam("blob_filter_min_blob_area", blob_detector_.params_.minArea);
+        updateParam("blob_filter_max_blob_area", blob_detector_.params_.maxArea);
+        updateParam("blob_filter_border_margin", blob_detector_.params_.border_margin);
 
         //: other params:
         updateParam("use_bw_result", result_image_maker_.params_.use_bw_result_);
@@ -272,16 +325,32 @@ public:
     void processImage(cv::Mat const& src_image, std::string const& image_file_name = "") const {
         boost::posix_time::ptime time1 = boost::posix_time::microsec_clock::local_time();
         cv::Mat obj_mask = mask_maker_.makeObjectMask(src_image);
-        std::vector<cv::KeyPoint> keypoints;
-        blob_detector_.detect(obj_mask, keypoints);
+        BlobDetector::Contours blob_contours = blob_detector_.findBlobContours(obj_mask);
         boost::posix_time::ptime time2 = boost::posix_time::microsec_clock::local_time();
-        ROS_INFO("TRACE(test_simple_image_process):         done in %f milliseconds, %d blobs found.", (time2 - time1).total_microseconds() / 1000., (int)keypoints.size());
 
         if(result_image_consumer_.willUseResultImage()) {
-            cv::Mat res_image = result_image_maker_.makeStitchedResultImage(src_image, obj_mask, keypoints);
+            cv::Mat res_image = result_image_maker_.makeStitchedResultImage(src_image, obj_mask, blob_contours);
             result_image_consumer_.consumeResultImage(res_image, image_file_name);
         }
-        //fixme: publish keypoints
+        boost::posix_time::ptime time3 = boost::posix_time::microsec_clock::local_time();
+        ROS_INFO("TRACE(test_simple_image_process):         processed in %f milliseconds, %d blobs found, %fms took drawing debug picture."
+                 , (time2 - time1).total_microseconds() / 1000.
+                 , (int)blob_contours.size()
+                 , (time3 - time2).total_microseconds() / 1000.);
+
+        if(blob_contours.size() > 0) {
+            //fixme: publish first candidate point for now:
+            //fixme: not good message type
+            cv::Rect bbox = cv::boundingRect(blob_contours[0]);
+            cv::Point res_point((bbox.br().x + bbox.tl().x) / 2, (bbox.br().y + bbox.tl().y) / 2);
+            geometry_msgs::Point result;
+            result.x = res_point.x;
+            result.y = res_point.y;
+            result.z = 0;
+            //result.hits = 5;
+            res_pub_.publish(result);
+            ROS_INFO("publish result %d,%d", res_point.x, res_point.y);
+        }
     }
 
     void monoImageCb(const sensor_msgs::ImageConstPtr& image_msg) {
@@ -294,6 +363,12 @@ public:
           return;
         }
         processImage(image_ptr->image);
+
+//        static char prefix = 'a';
+//        if(prefix < 'z') {
+//            std::string path_to_save = std::string("/home/dd/img2dir_sources/ground/") + prefix++ + ".jpg";
+//            cv::imwrite(path_to_save, image_ptr->image);
+//        }
     }
 
     template<typename t_Param>
@@ -305,9 +380,10 @@ private:
     static boost::filesystem::directory_iterator findNextImage(boost::filesystem::directory_iterator input_image_itr) {
         boost::filesystem::directory_iterator end_itr;
         while(input_image_itr != end_itr
-            && !boost::filesystem::is_regular_file(input_image_itr->path())
-            && input_image_itr->path().string().size() < 4
-            && input_image_itr->path().string().substr(input_image_itr->path().string().size() - 4, 4) != ".jpg"
+            && (!boost::filesystem::is_regular_file(input_image_itr->path())
+                || input_image_itr->path().string().size() < 4
+                || input_image_itr->path().string().substr(input_image_itr->path().string().size() - 4, 4) != ".jpg"
+               )
             ) ++input_image_itr;
         return input_image_itr;
     }
